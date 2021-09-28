41999825ca5ac8ef3d5b2c9ad08482294fdcb65a
Sohail Mahmud <sohailmahmuud@gmail.com>
2021-09-29 00:11:10 +0600

    update wtest

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_settings_example/main.dart';
+import 'package:app_settings_manager/main.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
